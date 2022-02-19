b98c54c1d66b2186918e8932a571989e6b9e3bf7
xCriminalStylex <95683571+xCriminalStylex@users.noreply.github.com>
2022-02-19 08:51:31 +0200

    Update NewFunction.cs

--- AutoTestForCICD/NewFunction.cs
+++ AutoTestForCICD/NewFunction.cs
@@ -10,9 +10,7 @@ namespace AutoTestForCICD
     {
         public static int MultiplicationPlusSquare(int a, int b, int c, int d)
         {     
-            
-            
-            
+                       
             int result = a * b - c * d;
             return result;            
         }
